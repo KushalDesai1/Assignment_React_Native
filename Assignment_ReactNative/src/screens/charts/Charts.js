5fd1f1d1b22bdd760cc70175ce07469c2aeac973
Kushal Desai <kushaldesai1990@gmail.com>
2020-08-22 20:33:07 +0530

    Added BarChart for 'Age Range Data' in Charts screen

--- Assignment_ReactNative/src/screens/charts/Charts.js
+++ Assignment_ReactNative/src/screens/charts/Charts.js
@@ -1,21 +1,57 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Modal, Text} from 'react-native';
 import HeaderComponent from '../../components/HeaderComponent/HeaderComponent';
 import {ChartStyles} from './ChartStyle';
 import AsyncStorage from '@react-native-community/async-storage';
+import LoaderComponent from '../../components/LoaderComponent/LoaderComponent';
+import APIStrings from '../../api/APIStrings';
+import AppStrings from '../../utils/AppStrings';
+import {BarChart, Grid} from 'react-native-svg-charts';
 
 class Charts extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-        participantList: [],
+      participantList: [],
+      isLoading: false,
+      ageRangeData: [],
+      pieChartData: [],
     };
   }
 
   async componentDidMount() {
-    const participantData = await AsyncStorage.getItem("ParticipantData");
-    this.setState({ participantList: participantData })
+    const participantData = await AsyncStorage.getItem('ParticipantData');
+
+    if (participantData !== null) {
+      this.setState({participantList: participantData}, () => {
+        alert(participantData);
+      });
+    } else {
+      this.setState({isLoading: true}, () => this.callParticipantAPI());
+    }
   }
+
+  reports = () => {
+    let arrayHolder = this.state.participantList;
+
+    const randomColor = () =>
+      ('#' + ((Math.random() * 0xffffff) << 0).toString(16) + '000000').slice(
+        0,
+        7,
+      );
+
+    const barData1 = arrayHolder.filter(
+      (value) => value.age >= 13 && value.age <= 18,
+    ).length;
+    const barData2 = arrayHolder.filter(
+      (value) => value.age > 18 && value.age <= 25,
+    ).length;
+    const barData3 = arrayHolder.filter((value) => value.age > 25).length;
+
+    const barData = [barData1, barData2, barData3];
+
+    this.setState({ageRangeData: barData});
+  };
 
   callParticipantAPI = async () => {
     try {
@@ -30,7 +66,9 @@ class Charts extends React.Component {
       let responseJson = await response.json();
       this.setState({isLoading: false});
       if (responseJson.length > 0) {
-        this.setState({participantList: responseJson});
+        this.setState({participantList: responseJson}, () => {
+          this.reports();
+        });
       }
     } catch (error) {
       this.setState({
@@ -41,10 +79,31 @@ class Charts extends React.Component {
   };
 
   render() {
+    const fill = 'rgb(240, 18, 18)';
     return (
       <View style={ChartStyles.rootViewContainer}>
-        <HeaderComponent title="Charts" />
-        <Text>Charts Screen</Text>
+        <HeaderComponent title="Reports" />
+        <Modal
+          animationType="fade"
+          transparent={true}
+          visible={this.state.isLoading}
+          onRequestClose={() => {}}>
+          <LoaderComponent loading={this.state.isLoading} />
+        </Modal>
+        <Text>Number of people in a given age range</Text>
+        <BarChart
+          style={{height: 200, width: '50%', margin: 20}}
+          data={this.state.ageRangeData}
+          yAccessor = {({item}) => item}
+          svg={{fill}}
+          contentInset={{top: 30, bottom: 30}}>
+          <Grid />
+        </BarChart>
+        <Text>Number of people by localities</Text>
+        <Text>
+          Average group size of people attending the event(using guest count)
+        </Text>
+        <Text>Professionals & students count</Text>
       </View>
     );
   }
